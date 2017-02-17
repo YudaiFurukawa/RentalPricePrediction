d7892581aa7cc605b5645880cb1d141a36845873
unknown <>
2017-02-17 20:10:25 +0900

    feat: add scatter matrix

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 ###import
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
 import json
 import pandas as pd
 
@@ -12,11 +15,32 @@ data = pd.read_json('train.json')
 data.drop(['price'],axis = 1, inplace = True)
 print 'price is separated from data'
 
-###display data 
+###display data stats
 from IPython.display import display
 display(data.describe())
+
+###drop features
+#features not needed
+del data['photos'], data['manager_id'], data['listing_id'], data['description'],data['building_id'], data['created']
+#features maybe needed
+del data['features'], data['display_address'],data['street_address'],data['interest_level']
+print data.keys()
+
+###reset index
+data = data.reset_index(drop=True)
+
+###pick some samples 
+#print samples
+indices = [30,150,95]
+# Create a DataFrame of the chosen samples
+samples = pd.DataFrame(data.loc[indices], columns = data.keys())
+print "Chosen samples:"#from matplotlib import rcParams, rcParamsDefault, get_backend
+#
+display(samples)
+
+#from matplotlib import rcParams, rcParamsDefault, get_backend
+from pandas.tools.plotting import scatter_matrix
+scatter_matrix(data, alpha = 0.3, figsize = (6,6), diagonal = 'kde');
+plt.show()
 ###
 
-###print sample
-# indices = [86,150,95]
-# samples = pd.DataFrame(data.loc[indices], columns = data.keys()).reset_index(drop = True)
